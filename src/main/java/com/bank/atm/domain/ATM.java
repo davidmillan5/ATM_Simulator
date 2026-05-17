bfed38fc9f3b3b0cf6f952a4617044418ab1e841
Cesar David Millan <cesardavidmillan5@gmail.com>
2026-05-16 19:07:51 -0500

    working on card class

--- src/main/java/com/bank/atm/domain/ATM.java
+++ src/main/java/com/bank/atm/domain/ATM.java
@@ -1,4 +1,57 @@
 package com.bank.atm.domain;
 
+import java.math.BigDecimal;
+import java.util.UUID;
+
 public class ATM {
+
+    // Generate a random UUID
+    UUID uuid = UUID.randomUUID();
+
+    private String id = uuid.toString();
+    private String location;
+    private BigDecimal cashAvailable = BigDecimal.valueOf(0.00);
+    private Card inserCard;
+    private Account currentSessionAccount;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public BigDecimal getCashAvailable() {
+        return cashAvailable;
+    }
+
+    public void setCashAvailable(BigDecimal cashAvailable) {
+        this.cashAvailable = cashAvailable;
+    }
+
+    public Card getInserCard() {
+        return inserCard;
+    }
+
+    public void setInserCard(Card inserCard) {
+        this.inserCard = inserCard;
+    }
+
+    public Account getCurrentSessionAccount() {
+        return currentSessionAccount;
+    }
+
+    public void setCurrentSessionAccount(Account currentSessionAccount) {
+        this.currentSessionAccount = currentSessionAccount;
+    }
+
+
+
+
+
 }
--- src/main/java/com/bank/atm/domain/Card.java
+++ src/main/java/com/bank/atm/domain/Card.java
@@ -1,4 +1,91 @@
 package com.bank.atm.domain;
 
+import java.time.LocalDate;
+
 public class Card {
+
+    private String cardNumber;
+    private String pin;
+    private boolean blocked;
+    private int failedAttempts = 0;
+    private LocalDate expirationDate;
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    public boolean isBlocked() {
+        return blocked;
+    }
+
+    public void setBlocked(boolean blocked) {
+        this.blocked = blocked;
+    }
+
+    public int getFailedAttempts() {
+        return failedAttempts;
+    }
+
+    public void setFailedAttempts(int failedAttempts) {
+        this.failedAttempts = failedAttempts;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+
+    String validatePin(String pin){
+        String message = "";
+        String actualPin = getPin();
+        if(actualPin.equals(pin)){
+            message = "Access Granted";
+        }
+        return message;
+    }
+
+    void incrementFailedAttemps(){
+        while((getFailedAttempts() > 0 && getFailedAttempts() <= 3)){
+            if(!validatePin(pin).equals(pin)){
+                failedAttempts++;
+                setFailedAttempts(failedAttempts);
+                System.out.println("Your PIN is incorrect. Enter a valid PIN");
+            }else if(getFailedAttempts() == 3) {
+                blocked = true;
+                block();
+            }else{
+                System.out.println("Transaction approved.");
+                resetAttempts();
+                break;
+            }
+        }
+    }
+
+    void resetAttempts(){
+        setFailedAttempts(0);
+    }
+
+    void block(){
+        if(blocked){
+            System.out.println("Your card with number " + getCardNumber() + " has being blocked!");
+        }
+
+    }
+
 }
